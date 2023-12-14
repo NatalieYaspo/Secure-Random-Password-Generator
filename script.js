8c1617e0e22d6adbcf35343f5f99caab16d0b9ba
Traspo <nyaspo8@gmail.com>
2023-12-13 20:05:28 -0500

    added code for arrays and prompts

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
+var UPPERCASE = [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
+var numeric = [...Array(10)].map((_, i) => String.fromCharCode(i + 48));
+var special = [...Array(15)].map((_, i) => String.fromCharCode(i + 33));
+console.log(lowercase);
+console.log(UPPERCASE);
+console.log(numeric);
+console.log(special);
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +19,14 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword) {
+  // var passwordLength = prompt("How long should this password be?", "8-128 characters");
+  // var isLowercase = prompt("Should password include lowercase letters? (Ok = yes, Cancel = No");
+  // var isUppercase = prompt("Should password include UPPERCASE letters? (Ok = yes, Cancel = No");
+  // var isNumeric = prompt("Should password include any numbers? (Ok = yes, Cancel = No");
+  // var isSpecial = prompt("Should password include any special characters? (Ok = yes, Cancel = No");
+
+
+
+// console.log(passwordLength);
+// console.log(isLowercase);
